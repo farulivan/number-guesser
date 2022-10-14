9a5a52b1cdc08dc3ec53eba37342734ae5033162
Farul Ivan <farulivan@gmail.com>
2022-10-14 13:14:22 +0700

    add some codes for script

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => Math.floor(Math.random() * 10)
+console.log(generateTarget())
+const compareGuesses = (human, computer, secret) => {
+  const humanDiff = secret - human
+  const computerDiff = secret - computer
+  return humanDiff < computerDiff ? true : false
+}
+console.log(compareGuesses(1,3,5))
+
+const updateScore = (str) => {
+  if(str === 'human'){
+    return humanScore + 1
+  }
+  return computerScore + 1
+}
+
+const advanceRound = () => {
+  return currentRoundNumber + 1
+}
