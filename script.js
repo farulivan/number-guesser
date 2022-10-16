4b7111dc5915ac7ade5882b64daa92cd411259e3
Farul Ivan <farulivan@gmail.com>
2022-10-16 09:24:07 +0700

    fix some codes

--- script.js
+++ script.js
@@ -3,23 +3,27 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
-
-const generateTarget = () => Math.floor(Math.random() * 10)
-console.log(generateTarget())
+let secretNumber = 0
+const generateTarget = () => { 
+  secretNumber = Math.floor(Math.random() * 10)
+}
+generateTarget()
+console.log(secretNumber)
 const compareGuesses = (human, computer, secret) => {
-  const humanDiff = secret - human
-  const computerDiff = secret - computer
+  const humanDiff = Math.abs(secret - human)
+  const computerDiff = Math.abs(secret - computer)
+  console.log(humanDiff, computerDiff)
   return humanDiff < computerDiff ? true : false
 }
-console.log(compareGuesses(1,3,5))
+console.log(compareGuesses(1,3,secretNumber))
 
 const updateScore = (str) => {
   if(str === 'human'){
-    return humanScore + 1
+    humanScore = humanScore + 1
   }
-  return computerScore + 1
+  computerScore = computerScore + 1
 }
 
 const advanceRound = () => {
-  return currentRoundNumber + 1
+  currentRoundNumber = currentRoundNumber + 1
 }
